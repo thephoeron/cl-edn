6804a04577b334700b79f2fadbd11af13d9a9c12
the Phoeron <thephoeron@users.noreply.github.com>
2015-08-30 11:05:14 -0400

    Add sanity check test

--- t/cl-edn.lisp
+++ t/cl-edn.lisp
@@ -7,4 +7,24 @@
 
 (in-package :cl-edn-test)
 
+;; NOTE: To run this test file, execute `(asdf:test-system :cl-edn)' in your Lisp.
+
+(plan 1)
+
+(deftest sanity-check
+  (pass "PROVE is loaded and ready to go.")
+  (ok (= 1 1)
+      "Numeric equality: (= 1 1) => T.")
+  (is (+ 1 1)
+      2
+      "Addition: (+ 1 1) => 2.")
+  (is (* 2 2)
+      4
+      "Multiplication: (* 2 2) => 4.")
+  (is (mod (+ 10 2) 10)
+      2
+      "Modulus: (mod (+ 10 2) 10) => 2."))
+
+(run-test-all)
+
 ;; EOF
